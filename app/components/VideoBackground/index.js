c9adb96574d8f92938e81fd458b859e3b50926fa
ravi vasoya <ravi.vasoya@sublimedatasys.com>
2018-09-24 20:17:08 +0530

    video tested on ios-safari, moz, chrome playing in all except mobile chrome

--- app/components/VideoBackground/index.js
+++ app/components/VideoBackground/index.js
@@ -2,10 +2,31 @@ import React, { Component } from 'react';
 import videoFile from 'video/video_banner.mp4';
 
 class VideoBackground extends Component { // eslint-disable-line react/prefer-stateless-function
+  componentDidMount() {
+    // this.checkforVideo();
+    // this.play(true);
+  }
+
+  play(flag) {
+    const promise = document.querySelector('video').play();
+    if (promise !== undefined) {
+      promise.then(() => {
+        console.log('video started!');
+      }).catch(() => {
+        console.log('Autoplay was prevented.');
+        if (flag) {
+          setTimeout(() => {
+            this.play();
+          }, 1500);
+        }
+        // Show a "Play" button so that user can start playback.
+      });
+    }
+  }
   render() {
     return (
       <div className="video-container">
-        <video autoPlay loop>
+        <video loop playsInline autoPlay muted>
           <source src={videoFile} type="video/mp4" />
         </video>
       </div>
